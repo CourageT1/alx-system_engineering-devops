d06c342f4de0d39c5e8ed41ac074891d7ba7634c
CourageT1 <tcgwizhi@gmail.com>
2023-08-01 11:36:56 -0700

    ididmybest

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,20 @@
-#!/usr/bin/env bash
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+#!/usr/bin/env ruby
+def extract_info(line)
+  sender = line.match(/\[from:(\S+)\]/)&.captures&.first
+  receiver = line.match(/\[to:(\S+)\]/)&.captures&.first
+  flags = line.match(/\[flags:(.*?)\]/)&.captures&.first
+
+  "#{sender},#{receiver},#{flags}"
+end
+if ARGV.empty?
+  puts "Please provide the log file."
+else
+  log_file_path = ARGV[0]
+
+  File.readlines(log_file_path).each do |line|
+    if line.include?("SMS")
+      result = extract_info(line)
+      puts result unless result.nil?
+    end
+  end
+end
