05f566ca52d972fcff370f54cf29f0664f4cfb6c
CourageT1 <tcgwizhi@gmail.com>
2023-09-22 12:00:57 -0700

    correcting2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
+# Using Puppet, install flask from pip3.
+
 exec { 'kill_killmenow':
   command     => 'pkill killmenow',
   path        => ['/bin', '/usr/bin'], # Specify the command's path as needed
